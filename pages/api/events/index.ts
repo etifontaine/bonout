c631a5339d4432373be1c575f1ba5cbf5d07eccd
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-13 17:58:46 +0100

    feat: post an event

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,11 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { BoEvent } from "../../../src/types";
-import { getEvents } from "../../../src/models/events";
+import {
+  getEvents,
+  createEvent,
+} from "../../../src/models/events";
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<BoEvent[]>
+  res: NextApiResponse<
+    BoEvent[] | { error: string } | { message: string }
+  >
 ) {
-  const events = await getEvents();
-  if (!events) throw new Error("No events found");
-  res.status(200).json(events);
+  if (req.method === "GET") {
+    const events = await getEvents();
+    if (!events)
+      return res
+        .status(500)
+        .json({ error: "Failed to get events" });
+
+    res.status(200).json(events);
+  }
+
+  if (req.method === "POST") {
+    const payload = JSON.parse(req.body);
+    await createEvent(payload)
+      .then(() => {
+        res.status(204).json({ message: "Success" });
+      })
+      .catch(e => {
+        res.status(500).json({ error: e.message });
+      });
+  }
 }
