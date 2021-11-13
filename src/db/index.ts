8887ada8116495537bf47e211fe8a206c57374a9
Etienne Fontaine <etienne.fontaine1@gmail.com>
2021-11-13 21:11:09 +0100

    Feat firebase cloudrun (#6)
    * [feat] init Firebase based on env

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,15 @@ import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
     try {
-        admin.initializeApp({
-            credential: admin.credential.cert('bonout-web-app-sa.json'),
-        });
+        if (process.env.DB_ENV === 'production') {
+            admin.initializeApp({
+                serviceAccountId: process.env.SA_ID
+            })
+        } else {
+            admin.initializeApp({
+                credential: admin.credential.cert('bonout-web-app-sa.json'),
+            });
+        }
     } catch (error) {
         console.log('Firebase admin initialization error', error);
     }
