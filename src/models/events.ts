2612995ebafdd9604a5456ae5cb2934cb7c0d236
Etienne Fontaine <etienne.fontaine@blablacar.com>
2021-11-13 18:22:15 +0100

    [feat] Setup Firestore

--- src/models/events.ts
+++ src/models/events.ts
@@ -1,8 +1,11 @@
-import { getDB } from "./db";
+import db from '../db';
 import { BoEvent } from "../types";
 
+
 export async function getEvents(): Promise<BoEvent[]> {
-  const db = await getDB();
-  const events = db.events;
-  return events || [];
+
+  const eventsQuery = await db.collection('events').get()
+  const events = eventsQuery.docs.map(x => x.data() as BoEvent)
+
+  return events || []
 }
