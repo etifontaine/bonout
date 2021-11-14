536e72672a9dc5fcdde7b0039e9dbeba41e3929e
Etienne Fontaine <etienne.fontaine1@gmail.com>
2021-11-14 14:34:44 +0100

    [feat] Add Invitation response (#9)

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type BoEvent = {
-  id?: string;
+  id: string;
   title: string;
   description: string;
   user_id: string;
@@ -8,8 +8,22 @@ export type BoEvent = {
   end_at: string;
   main_asset?: string;
   link: string;
+  invitations: BoInvitationResponse[]
 };
 
 export type BoDB = {
   events: BoEvent[];
 };
+
+export enum BoInvitationValidResponse {
+  YES = 'yes',
+  NO = 'no',
+  MAYBE = 'maybe',
+}
+
+export type BoInvitationResponse = {
+  name: string
+  link: string
+  response: BoInvitationValidResponse
+  created_at: string
+}
