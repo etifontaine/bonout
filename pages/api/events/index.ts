8f785acd4159ae8f8461eaf7c20b3f1b24b14282
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-15 13:21:28 +0100

    refacto(api) : events index

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,47 +1,60 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { v4 as uuidv4 } from "uuid";
 import type { BoEvent } from "../../../src/types";
-import {
-  getEvents,
-  createEvent,
-  getEventByLink,
-} from "../../../src/models/events";
+import { getEvents, createEvent } from "../../../src/models/events";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<BoEvent[] | { error: string } | { message: string }>
 ) {
-  if (req.method === "GET") {
-    const events = await getEvents();
-    if (!events) return res.status(500).json({ error: "Failed to get events" });
-
-    return res.status(200).json(events);
+  try {
+    await getEventsHandler();
+    await createEventHandler();
+  } catch (e: any) {
+    console.error(e);
+    res.status(500).json({ error: e.message });
   }
 
-  if (req.method === "POST") {
-    if (!req.body) {
+  async function getEventsHandler() {
+    if (isNotGetRequest()) return;
+
+    return res.status(200).json(await getEvents());
+
+    function isNotGetRequest(): boolean {
+      return req.method !== "GET";
+    }
+  }
+
+  async function createEventHandler() {
+    if (isNotPostRequest()) return;
+    if (isBodyEmpty())
       return res.status(400).json({ error: "body must be set" });
-    }
 
-    let payload: BoEvent | null = null;
-    try {
-      payload = JSON.parse(req.body);
-    } catch (error) {
-      return res.status(400).json({ error: "can not parse body" });
-    }
+    const payload = getBodyPayload();
+    if (!payload) return res.status(400).json({ error: "payload must be set" });
 
-    if (!payload) {
-      return res.status(400).json({ error: "payload must be set" });
-    }
-
-    payload.link = uuidv4();
-
-    return await createEvent(payload)
+    return await createEvent(setUniqLink(payload))
       .then((event) => {
         return res.status(201).json({ message: event.link });
       })
       .catch((e) => {
         return res.status(500).json({ error: e.message });
       });
+
+    function isNotPostRequest(): boolean {
+      return req.method !== "POST";
+    }
+
+    function isBodyEmpty(): boolean {
+      return !req.body;
+    }
+
+    function getBodyPayload(): BoEvent {
+      return JSON.parse(req.body);
+    }
+
+    function setUniqLink(payload: BoEvent): BoEvent {
+      return { ...payload, link: uuidv4() };
+    }
   }
 }
