61a4cba5f03e5432ffb3da0042d7296467b44916
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-16 19:50:41 +0100

    feat(utils): add errorMessages contant

---
+++ src/utils/errorMessages.ts
@@ -0,0 +1,8 @@
+export const API_ERROR_MESSAGES = {
+  BODY_EMPTY: "Body is empty",
+  BODY_NOT_JSON: "Body is not a valid json",
+  SHOULD_BE_STRING: " should be a string",
+  SHOULD_NOT_BE_EMPTY: " should not be empty",
+  PROPERTY_NOT_FOUND: " property not found",
+  NOT_FOUND: " not found",
+};
