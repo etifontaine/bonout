b6d637d5bbd6baddf681d1ed49ae568084938978
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-16 19:50:16 +0100

    fix(api.events.index): to pass tests

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -2,11 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { v4 as uuidv4 } from "uuid";
 import type { BoEvent } from "../../../src/types";
 import { RequestError } from "../../../src/utils/CustomErrors";
+import { API_ERROR_MESSAGES } from "../../../src/utils/errorMessages";
 import { getEvents, createEvent } from "../../../src/models/events";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<BoEvent[] | { error: string } | { message: string }>
+  res: NextApiResponse<BoEvent[] | { error: string } | BoEvent>
 ) {
   try {
     if (isGetRequest())
@@ -14,7 +15,7 @@ export default async function handler(
 
     if (isPostRequest())
       await createEventHandler().then((event) => {
-        return res.status(201).json({ message: event.link });
+        return res.status(201).json(event);
       });
   } catch (err: any) {
     if (err instanceof RequestError) {
@@ -41,47 +42,72 @@ export default async function handler(
     }
 
     function getBodyPayload(): BoEvent {
-      if (isBodyEmpty()) throw new RequestError("Body is empty");
-      return checkEventTypes(checkJSONSyntax(req.body));
+      return checkEventTypes(
+        checkIfPorpertyMissing(checkJSONSyntax(checkIfBodyEmpty(req.body)))
+      );
 
-      function isBodyEmpty(): boolean {
-        return !req.body;
+      function checkIfBodyEmpty(body: string): string {
+        if (!body) throw new RequestError(API_ERROR_MESSAGES.BODY_EMPTY);
+        return body;
       }
+
       function checkJSONSyntax(json: string) {
         try {
           return JSON.parse(json);
         } catch (err) {
-          throw new RequestError("Body is not a valid JSON");
+          throw new RequestError(API_ERROR_MESSAGES.BODY_NOT_JSON);
+        }
+      }
+
+      function checkIfPorpertyMissing(event: BoEvent) {
+        const missingProperty = fieldsToCheck().filter(isPropertyMissing);
+        if (missingProperty.length > 0) {
+          throw new RequestError(
+            missingProperty.join(", ") + API_ERROR_MESSAGES.PROPERTY_NOT_FOUND
+          );
+        }
+        return event;
+
+        function isPropertyMissing(field: string): boolean {
+          return Object.keys(event).indexOf(field) === -1;
         }
       }
 
       function checkEventTypes(event: BoEvent): BoEvent {
-        fieldsToCheck().forEach((field) => {
-          if (!isString(field))
-            throw new RequestError(`${field} must be a string !`);
-          if (isEmpty(field))
-            throw new RequestError(`${field} must not be empty !`);
-        });
+        Object.entries(event)
+          .map(([key, value]) => ({
+            key,
+            value,
+          }))
+          .forEach((field) => {
+            if (!isString(field.value))
+              throw new RequestError(
+                field.key + API_ERROR_MESSAGES.SHOULD_BE_STRING
+              );
+            if (isEmpty(field.value))
+              throw new RequestError(
+                field.key + API_ERROR_MESSAGES.SHOULD_NOT_BE_EMPTY
+              );
+          });
 
         return event;
-
-        function fieldsToCheck(): string[] {
-          return [
-            event.title,
-            event.description,
-            event.address,
-            event.start_at,
-            event.end_at,
-            event.user_id,
-          ];
-        }
 
         function isString(element: any): boolean {
           return typeof element === "string";
         }
-        function isEmpty(s: string): boolean {
+        function isEmpty(s: any): boolean {
           return !s || !s.trim();
         }
+      }
+      function fieldsToCheck() {
+        return [
+          "title",
+          "description",
+          "address",
+          "start_at",
+          "end_at",
+          "user_id",
+        ];
       }
     }
   }
