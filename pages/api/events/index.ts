fbe781f891bbea8737c20c2c0b81b284b319b059
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-16 14:33:55 +0100

    refacto(api): better error handling

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { v4 as uuidv4 } from "uuid";
 import type { BoEvent } from "../../../src/types";
+import { RequestError } from "../../../src/utils/CustomErrors";
 import { getEvents, createEvent } from "../../../src/models/events";
 
 export default async function handler(
@@ -8,53 +9,47 @@ export default async function handler(
   res: NextApiResponse<BoEvent[] | { error: string } | { message: string }>
 ) {
   try {
-    await getEventsHandler();
-    await createEventHandler();
-  } catch (e: any) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  }
+    if (isGetRequest())
+      await getEvents().then((events) => res.status(200).json(events));
 
-  async function getEventsHandler() {
-    if (isNotGetRequest()) return;
-
-    return res.status(200).json(await getEvents());
-
-    function isNotGetRequest(): boolean {
-      return req.method !== "GET";
-    }
-  }
-
-  async function createEventHandler() {
-    if (isNotPostRequest()) return;
-    if (isBodyEmpty())
-      return res.status(400).json({ error: "body must be set" });
-
-    const payload = getBodyPayload();
-    if (!payload) return res.status(400).json({ error: "payload must be set" });
-
-    return await createEvent(setUniqLink(payload))
-      .then((event) => {
+    if (isPostRequest())
+      await createEventHandler().then((event) => {
         return res.status(201).json({ message: event.link });
-      })
-      .catch((e) => {
-        return res.status(500).json({ error: e.message });
       });
-
-    function isNotPostRequest(): boolean {
-      return req.method !== "POST";
+  } catch (err: any) {
+    if (err instanceof RequestError) {
+      res.status(400).json({ error: err.message });
+    } else {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
     }
+  }
+
+  function isGetRequest(): boolean {
+    return req.method === "GET";
+  }
+
+  function isPostRequest(): boolean {
+    return req.method === "POST";
+  }
+
+  async function createEventHandler(): Promise<BoEvent> {
+    if (isBodyEmpty()) throw new RequestError("Body is empty");
+
+    return await createEvent(setUniqLink(getBodyPayload()));
 
     function isBodyEmpty(): boolean {
       return !req.body;
     }
 
-    function getBodyPayload(): BoEvent {
-      return JSON.parse(req.body);
-    }
-
     function setUniqLink(payload: BoEvent): BoEvent {
       return { ...payload, link: uuidv4() };
+    }
+
+    function getBodyPayload(): BoEvent {
+      const payload = JSON.parse(req.body);
+      if (!payload) throw new RequestError("Body is not a valid JSON");
+      return payload;
     }
   }
 }
