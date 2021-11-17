36432ac90e14bfc37a3a687246de0c94cf872ab3
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-17 15:09:07 +0100

    feat(errorMessages): toLowerCase

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -1,11 +1,11 @@
 export const API_ERROR_MESSAGES = {
-  BODY_EMPTY: "Body is empty",
-  BODY_NOT_JSON: "Body is not a valid json",
+  BODY_EMPTY: "body is empty",
+  BODY_NOT_JSON: "body is not a valid json",
   SHOULD_BE_STRING: " should be a string",
   SHOULD_NOT_BE_EMPTY: " should not be empty",
   PROPERTY_NOT_FOUND: " property not found",
   NOT_FOUND: " not found",
-  UNKNOWN_PARAMETER: "Unknown parameter",
+  UNKNOWN_PARAMETER: "unknown parameter",
   VALUE_MISSING: " value missing",
-  METHOD_NOT_ALLOWED: "Method not allowed",
+  METHOD_NOT_ALLOWED: "method not allowed",
 };
