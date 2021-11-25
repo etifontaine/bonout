b034fa308508098653a095c5865c2e8423beb1b4
Etienne Fontaine <etienne.fontaine@blablacar.com>
2021-11-25 14:32:09 +0100

    [feat] Add Login feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,92 +1,115 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { getUserID } from "src/utils/user";
+import LoginModal from "./LoginModal";
 
 export default function Header() {
+
+  const [user, setUser] = useState("")
+  const [isLoginVisible, setLoginVisible] = useState(false)
+
+  useEffect(() => {
+    const user = getUserID()
+    if (user) {
+      setUser(user);
+    }
+  }, [])
+
   return (
-    <Disclosure as="nav" className="bg-white-800">
-      {({ open }) => (
-        <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-yellow-400 hover:text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex-shrink-0 flex items-center cursor-pointer">
-                  <Link href={getUserID() ? "/home" : ""}>
-                    <Image
-                      alt="logo"
-                      src="/images/logo.svg"
-                      width="40"
-                      height="40"
-                      priority={true}
-                    />
-                  </Link>
+    <>
+      <LoginModal isVisible={isLoginVisible} setLoginVisible={setLoginVisible} />
+      <Disclosure as="nav" className="bg-white-800">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-yellow-400 hover:text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
                 </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <div className="hidden sm:block sm:ml-6">
-                  <div className="flex space-x-4">
-                    <Link href="/home">
-                      <a className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md font-medium">
-                        Mes événements
-                      </a>
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex-shrink-0 flex items-center cursor-pointer">
+                    <Link href={user.length > 0 ? "/home" : ""}>
+                      <Image
+                        alt="logo"
+                        src="/images/logo.svg"
+                        width="40"
+                        height="40"
+                        priority={true}
+                      />
                     </Link>
-                    <Link href="/events/create">
-                      <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                        <span>Créer un événement</span>
-                        <svg
-                          className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
-                          viewBox="0 0 12 12"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                            fillRule="nonzero"
-                          />
-                        </svg>
-                      </a>
-                    </Link>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <div className="hidden sm:block sm:ml-6">
+                    <div className="flex space-x-4">
+                      {user.length > 0 ? (
+                        <Link href="/home">
+                          <a className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md font-medium">
+                            Mes événements
+                          </a>
+                        </Link>
+                      ) : (
+                        <button onClick={() => setLoginVisible(true)} className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md font-medium">
+                          Connexion
+                        </button>
+                      )
+                      }
+                      <Link href="/events/create">
+                        <a className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                          <span>Créer un événement</span>
+                          <svg
+                            className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                            viewBox="0 0 12 12"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                              fillRule="nonzero"
+                            />
+                          </svg>
+                        </a>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <Disclosure.Panel className="sm:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {getUserID() ? (
+            <Disclosure.Panel className="sm:hidden border">
+              <div className="px-2 pt-2 pb-3 space-y-1">
+                {user.length > 0 ? (
+                  <Disclosure.Button
+                    as="a"
+                    href="/home"
+                    className="text-gray-600 hover:bg-gray-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    Mes événements
+                  </Disclosure.Button>
+                ) : null}
                 <Disclosure.Button
                   as="a"
-                  href="/home"
+                  href="/events/create"
                   className="text-gray-600 hover:bg-gray-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Mes événements
+                  Créer un événement
                 </Disclosure.Button>
-              ) : null}
-              <Disclosure.Button
-                as="a"
-                href="/events/create"
-                className="text-gray-600 hover:bg-gray-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Créer un événement
-              </Disclosure.Button>
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )
+        }
+      </Disclosure >
+    </>
   );
 }
---
+++ components/LoginModal.tsx
@@ -0,0 +1,139 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+import Router from "next/router";
+import { Dialog, Transition } from "@headlessui/react";
+import { ThumbUpIcon, LockOpenIcon } from "@heroicons/react/outline";
+import { toast } from "react-toastify";
+import { BoInvitationValidResponse } from "../src/types";
+
+interface IModal {
+  isVisible: boolean;
+  setLoginVisible: any
+}
+interface IModalForm {
+  user_id: string;
+}
+
+export default function InvitationModal({ isVisible, setLoginVisible }: IModal) {
+  const cancelButtonRef = useRef(null);
+  const [formContent, setFormContent] = useState<IModalForm>();
+
+  const postInvitationResponse = () => {
+    if (!formContent?.user_id) {
+      toast.error("Vous devez renseigner un user_id");
+      return;
+    }
+
+    localStorage.setItem("user_id", formContent.user_id);
+    Router.push('/home')
+  };
+
+  return (
+    <Transition.Root show={isVisible} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-40 inset-0 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        onClose={() => { }}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-75"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <LockOpenIcon
+                      className="h-6 w-6 text-green-600"
+                      aria-hidden="true"
+                    />
+                  </div>
+
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Connexion
+                    </Dialog.Title>
+                    <form
+                      className="bg-white pt-6"
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        postInvitationResponse();
+                      }}
+                    >
+                      <div className="mb-4">
+                        <label
+                          className="block text-gray-700 text-sm font-bold mb-2"
+                          htmlFor="user_id"
+                        >
+                          Renseignez votre user_id (pas de pseudo)
+                        </label>
+                        <input
+                          autoComplete="off"
+                          value={formContent?.user_id}
+                          onChange={(e) => {
+                            setFormContent({ user_id: e.target.value });
+                          }}
+                          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                          id="user_id"
+                          name="user_id"
+                          type="text"
+                          placeholder="user_id"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 hover:bg-green-700 focus:ring-green-500 text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm`}
+                  onClick={() => postInvitationResponse()}
+                >
+                  Confirmer
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setLoginVisible(false)}
+                  ref={cancelButtonRef}
+                >
+                  Annuler
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
