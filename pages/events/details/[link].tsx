361214d9527186e76ac9a16e8b6bf8f140ccdfc9
etifontaine <etifontaine@users.noreply.github.com>
2021-11-25 10:26:24 +0000

    Prettified Code!

--- pages/events/details/[link].tsx
+++ pages/events/details/[link].tsx
@@ -135,10 +135,11 @@ const EventDetails: NextPage<PageProps> = ({ event }) => {
               <div className="bg-gray-50 px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                 <dt className="text-sm font-medium text-gray-500">Lien</dt>
                 <div className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  <button onClick={() => shareEvent()}>{`${typeof window !== "undefined"
-                    ? `${window.location.host}/events/details/`
-                    : ""
-                    }${event.link}`}</button>
+                  <button onClick={() => shareEvent()}>{`${
+                    typeof window !== "undefined"
+                      ? `${window.location.host}/events/details/`
+                      : ""
+                  }${event.link}`}</button>
                 </div>
               </div>
               <div className="bg-white px-4 py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
