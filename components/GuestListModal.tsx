c7a44adae812dc70fd99c5121e89a265622f5891
Etienne Fontaine <etienne.fontaine@blablacar.com>
2021-11-25 22:27:20 +0100

    [fix] Fix modal GuestList width

--- components/GuestListModal.tsx
+++ components/GuestListModal.tsx
@@ -96,14 +96,14 @@ export default function GuestListModal({
                     />
                   </div>
 
-                  <div className="mt-3 sm:mt-0 sm:ml-4 sm:text-left">
+                  <div className="mt-3 sm:mt-0 sm:ml-4 w-full sm:text-left">
                     <Dialog.Title
                       as="h3"
-                      className="text-lg text-center mb-10 leading-6 font-medium text-gray-900"
+                      className="text-lg text-center mb-5 leading-6 font-medium text-gray-900"
                     >
                       Invités
                     </Dialog.Title>
-                    <div className="max-h-72 filter drop-shadow-lg overflow-y-auto">
+                    <div className="max-h-72 overflow-y-auto">
                       {renderGuestsList()}
                     </div>
                   </div>
