02b6f39105de7d9262c8e7bd5d4a16629993f311
Etienne Fontaine <etienne.fontaine1@gmail.com>
2021-11-26 15:43:08 +0100

    [feat] Toggle user_id on header (#67)
    * [feat] Toggle user_id on header

--- components/GuestListModal.tsx
+++ components/GuestListModal.tsx
@@ -55,7 +55,7 @@ export default function GuestListModal({
       <Dialog
         as="div"
         className="fixed z-40 inset-0 overflow-y-auto"
-        onClose={() => { }}
+        onClose={() => {}}
       >
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
@@ -86,7 +86,7 @@ export default function GuestListModal({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg w-60 sm:w-screen text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="inline-block align-bottom bg-white rounded-lg sm:w-96 w-screen text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
