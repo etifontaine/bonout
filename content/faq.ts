b3329f6d5b796bb246f22991ec35a3307503889d
etifontaine <etifontaine@users.noreply.github.com>
2021-11-27 18:44:51 +0000

    Prettified Code!

--- content/faq.ts
+++ content/faq.ts
@@ -1,19 +1,32 @@
-export const questions = [{
+export const questions = [
+  {
     title: "En quoi Bonout est différent?",
-    answer: "Bonout te permet d'organiser tes événements sans créer de compte, sans cookies, on respecte ta vie privée!"
-},{
+    answer:
+      "Bonout te permet d'organiser tes événements sans créer de compte, sans cookies, on respecte ta vie privée!",
+  },
+  {
     title: "Comment inviter des gens à mon événement?",
-    answer: "Une fois ton évènement crée, tu vas obtenir un lien unique. Partage ton lien par messagerie ou transforme le en QRCode afin d'inviter toutes les personnes que tu veux."
-},{
+    answer:
+      "Une fois ton évènement crée, tu vas obtenir un lien unique. Partage ton lien par messagerie ou transforme le en QRCode afin d'inviter toutes les personnes que tu veux.",
+  },
+  {
     title: "Comment êtes-vous rémunéré?",
-    answer: "Tu peux faire des dons, et plus tard on proposera des fonctionnalitées payantes. Toujours sans tracking et en respectant votre vie privée."
-},{
+    answer:
+      "Tu peux faire des dons, et plus tard on proposera des fonctionnalitées payantes. Toujours sans tracking et en respectant votre vie privée.",
+  },
+  {
     title: "Mes invités doivent-ils avoit un compte Bonout?",
-    answer: "Non, tu peux juste partager le lien de ton évènement et ils pourront répondre avec un pseudo ou leur nom."
-},{
+    answer:
+      "Non, tu peux juste partager le lien de ton évènement et ils pourront répondre avec un pseudo ou leur nom.",
+  },
+  {
     title: "Comment je fais pour me souvenir de mon user_id?",
-    answer: "Tu peux l'enregistrer dans ton gestionnaire de mot de passe ou dans une note. Sinon, tu peux en recréer un nouveau lors de ton prochain événement!"
-},{
+    answer:
+      "Tu peux l'enregistrer dans ton gestionnaire de mot de passe ou dans une note. Sinon, tu peux en recréer un nouveau lors de ton prochain événement!",
+  },
+  {
     title: "Existe-t-il une app Bonout?",
-    answer: "Pas pour l'instant. Si de plus en plus de monde utilise Bonout on pourra en créer une avec plaisir!"
-}]
+    answer:
+      "Pas pour l'instant. Si de plus en plus de monde utilise Bonout on pourra en créer une avec plaisir!",
+  },
+];
