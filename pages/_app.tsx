e395ded763458e0640d639ef7c3a60fbb3f1a8ce
etifontaine <etifontaine@users.noreply.github.com>
2021-11-28 15:30:19 +0000

    Prettified Code!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
-import NextNprogress from 'nextjs-progressbar';
+import NextNprogress from "nextjs-progressbar";
 import "../styles/style.scss";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -20,7 +20,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:title" content="Bonout" />
         <meta property="og:type" content="siteweb" />
-        <meta property="og:description" content="Organise ton prochain événement sans prise de tête, un seul site avec toutes les fonctionnalités!" />
+        <meta
+          property="og:description"
+          content="Organise ton prochain événement sans prise de tête, un seul site avec toutes les fonctionnalités!"
+        />
       </Head>
       <ToastContainer
         position="top-right"
@@ -33,7 +36,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         draggable
         pauseOnHover
       ></ToastContainer>
-      <NextNprogress color="#000000"/>
+      <NextNprogress color="#000000" />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import type { NextPage } from "next";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import Header from "@components/Header";
 import HeroHome from "@components/Landing/HeroHome";
 import HowItWorks from "@components/Landing/HowItWorks";
-const FeaturesHome = dynamic(() => import('@components/Landing/Features'))
-const FAQ = dynamic(() => import('@components/Landing/FAQ'))
-const Footer = dynamic(() => import('@components/Footer'))
+const FeaturesHome = dynamic(() => import("@components/Landing/Features"));
+const FAQ = dynamic(() => import("@components/Landing/FAQ"));
+const Footer = dynamic(() => import("@components/Footer"));
 import { getEventsCount } from "src/models/events";
 
 export async function getServerSideProps() {
