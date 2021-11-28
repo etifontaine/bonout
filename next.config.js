ec4b28c84d31d1473183cfd443d3c6c14b681d41
etifontaine <etifontaine@users.noreply.github.com>
2021-11-28 15:17:24 +0000

    Prettified Code!

--- next.config.js
+++ next.config.js
@@ -2,22 +2,22 @@
 
 const securityHeaders = [
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
   },
   {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload'
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
   },
   {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block'
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
   },
   {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN'
-  }
-]
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
 
 module.exports = {
   reactStrictMode: true,
@@ -26,9 +26,9 @@ module.exports = {
     return [
       {
         // Apply these headers to all routes in your application.
-        source: '/(.*)',
+        source: "/(.*)",
         headers: securityHeaders,
       },
-    ]
+    ];
   },
 };
