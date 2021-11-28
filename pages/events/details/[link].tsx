e3e7c71427958cd7bfe3650e70bb3b0a95e7544c
Antonin Carlin <antonin.carlin@icloud.com>
2021-11-28 17:37:30 +0100

    feat(useIsOrganizerOf): rename file

--- pages/events/details/[link].tsx
+++ pages/events/details/[link].tsx
@@ -6,7 +6,7 @@ import Head from "next/head";
 import dayjs from "dayjs";
 import Header from "@components/Header";
 import Modal from "@components/Invitation/Modal";
-import useIsOrganizerOfEvent from "@components/IsOrganizerOf";
+import useIsOrganizerOfEvent from "@components/useIsOrganizerOf";
 import GuestListModal from "@components/GuestListModal";
 import { BoEvent, BoInvitationValidResponse } from "src/types";
 import { getEventByLink } from "src/models/events";
