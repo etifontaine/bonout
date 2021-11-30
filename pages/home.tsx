41668edd7c6a9bf19336853cf94dd4e99ca2bc0c
Etienne Fontaine <etienne.fontaine@blablacar.com>
2021-11-30 22:19:35 +0100

    [feat] Add og:image

--- pages/home.tsx
+++ pages/home.tsx
@@ -33,14 +33,28 @@ const Home: NextPage = () => {
       <Head>
         <title>Bonout - Mes événements</title>
         <meta property="og:title" content="Bonout - Mes événements" />
+        <meta property="og:image" content={`${process.env.NEXT_PUBLIC_BASE_URL}/images/header-30112021.png`} />
         <meta
           name="description"
           content="Bonout t'aide à organiser ton prochain événement, un seul site avec toutes les fonctionnalités!"
         />
+        <link rel="apple-touch-icon" sizes="57x57" href="/logos/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/logos/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/logos/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/logos/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/logos/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/logos/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/logos/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/logos/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logos/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/logos/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logos/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/logos/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logos/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="/images/logo.svg" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#F3F2ED" />
       </Head>
       <Header />
       <section className="pt-24 md:mt-0 h-screen flex justify-center md:flex-row md:justify-between lg:px-48 md:px-12 px-4 bg-secondary">
