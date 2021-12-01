9773c9c053a200ad7758d959f36d2842aff36022
Antonin Carlin <antonin.carlin@icloud.com>
2021-12-01 21:25:50 +0100

    fix(types): english comment

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,7 @@ export type BoEvent = {
   title: string;
   description: string;
   user_id: string;
-  user_name?: string; // en option pour le moment etant donné que des données de prod ne l'ont pas
+  user_name?: string; // maybe undefined, because prod data have no user_name
   address: string;
   start_at: string;
   end_at: string;
