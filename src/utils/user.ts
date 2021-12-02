60e289c30585794c064c4c086612412ebb299585
Antonin Carlin <antonin.carlin@icloud.com>
2021-12-02 20:49:07 +0100

    chore(utils/user): remove console.log

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -2,7 +2,6 @@ import { isInstalled } from ".";
 
 export function getUserID() {
   const queries = getQueries();
-  console.log(queries);
   return typeof localStorage !== "undefined"
     ? localStorage.getItem("user_id")
     : isInstalled() && queries
