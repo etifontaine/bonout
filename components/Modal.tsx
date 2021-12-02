3be4ad2897103da4d2666ff87a268e0be3b609b3
Antonin Carlin <antonin.carlin@icloud.com>
2021-12-02 19:35:54 +0100

    feat(Modal): empty modal composent

---
+++ components/Modal.tsx
@@ -0,0 +1,110 @@
+import { Fragment, useEffect, useRef, useState } from "react";
+import Router from "next/router";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationIcon, ThumbUpIcon } from "@heroicons/react/outline";
+import { toast } from "react-toastify";
+
+interface IModalContent {
+  title: string;
+  description?: string;
+}
+interface IModalForm {
+  username: string;
+}
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  content: IModalContent;
+  children: React.ReactNode;
+  icon: React.ReactNode;
+}
+
+export default function Modal(props: ModalProps) {
+  const cancelButtonRef = useRef(null);
+  let [isOpen, setIsOpen] = useState(props.isOpen);
+
+  useEffect(() => {
+    setIsOpen(props.isOpen);
+  }, [props.isOpen]);
+
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed z-40 inset-0 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        onClose={() => {}}
+      >
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-75"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    {props.icon}
+                  </div>
+
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      {props.content?.title}
+                    </Dialog.Title>
+                    <Dialog.Description as="div" className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        {props.content?.description}
+                      </p>
+                    </Dialog.Description>
+                    {props.children}
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => {
+                    setIsOpen(false);
+                    props.onClose();
+                  }}
+                  ref={cancelButtonRef}
+                >
+                  Annuler
+                </button>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
