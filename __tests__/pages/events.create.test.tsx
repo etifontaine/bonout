ea51452dddf5a156f72750a57dfa7b484c327335
etifontaine <etifontaine@users.noreply.github.com>
2021-12-06 14:00:47 +0000

    Prettified Code!

--- __tests__/pages/events.create.test.tsx
+++ __tests__/pages/events.create.test.tsx
@@ -24,13 +24,13 @@ const getMaps = (type = "success", d = data): any => ({
           type === "opts"
             ? getPlacePredictions
             : (_: any, cb: (dataArg: any, status: string) => void) => {
-              setTimeout(() => {
-                cb(
-                  type === "success" ? d : null,
-                  type === "success" ? ok : error
-                );
-              }, 500);
-            };
+                setTimeout(() => {
+                  cb(
+                    type === "success" ? d : null,
+                    type === "success" ? ok : error
+                  );
+                }, 500);
+              };
       },
     },
   },
@@ -43,19 +43,19 @@ describe("CreateEventPage <Form />", () => {
   });
   describe("name input", () => {
     it("should exist", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#name");
       expect(input).toBeInTheDocument();
     });
 
     it("should have a label", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const label = result.container.querySelector("label[for='name']");
       expect(label).toBeInTheDocument();
     });
 
     it("should have a invalid class and a help-label if < 3 chars", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#name");
       if (input) {
         fireEvent.change(input, { target: { value: "a" } });
@@ -71,19 +71,19 @@ describe("CreateEventPage <Form />", () => {
 
   describe("description input", () => {
     it("should exist", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#description");
       expect(input).toBeInTheDocument();
     });
 
     it("should have a label", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const label = result.container.querySelector("label[for='description']");
       expect(label).toBeInTheDocument();
     });
 
     it("should have a invalid class and a help-label if < 5 chars", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#description");
       if (input) {
         fireEvent.change(input, { target: { value: "a" } });
@@ -99,19 +99,19 @@ describe("CreateEventPage <Form />", () => {
 
   describe("startAt Input", () => {
     it("should exist", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#startAt");
       expect(input).toBeInTheDocument();
     });
 
     it("should have a label", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const label = result.container.querySelector("label[for='startAt']");
       expect(label).toBeInTheDocument();
     });
 
     it("should have today datetime set + 10min", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#startAt");
       if (input) {
         expect(input).toHaveAttribute(
@@ -122,7 +122,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should have datetime > today dateime onChange", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#startAt");
       if (input) {
         const yesterday = new Date();
@@ -152,19 +152,19 @@ describe("CreateEventPage <Form />", () => {
 
   describe("endAt Input", () => {
     it("should exist", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#endAt");
       expect(input).toBeInTheDocument();
     });
 
     it("should have a label", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const label = result.container.querySelector("label[for='endAt']");
       expect(label).toBeInTheDocument();
     });
 
     it("should have today + 1h datetime set", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#endAt");
       if (input) {
         expect(input).toHaveAttribute(
@@ -175,7 +175,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should have datetime > startAt datetime onChange", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#endAt");
       const startAtInput = result.container.querySelector(
         "#startAt"
@@ -206,19 +206,19 @@ describe("CreateEventPage <Form />", () => {
 
   describe("location Input", () => {
     it("should exist", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#location");
       expect(input).toBeInTheDocument();
     });
 
     it("should have a label", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const label = result.container.querySelector("label[for='location']");
       expect(label).toBeInTheDocument();
     });
 
     it("should suggestion open onChange", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#location");
       fireLocationHelper(input);
       const suggestions = result.container.querySelector(".suggestions");
@@ -226,7 +226,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should be valid if an adress is selected", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector("#location");
       fireLocationHelper(input);
       expect(input).toHaveClass("invalid");
@@ -238,7 +238,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should be close suggestion if an adress is selected", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector(
         "#location"
       ) as HTMLInputElement;
@@ -252,7 +252,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should be valid if suggestion is cliked", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector(
         "#location"
       ) as HTMLInputElement;
@@ -272,7 +272,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should be close suggestion if user click outside", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector(
         "#location"
       ) as HTMLInputElement;
@@ -285,7 +285,7 @@ describe("CreateEventPage <Form />", () => {
     });
 
     it("should be re-open suggestion if user focus", () => {
-      const result = render(<Form onSubmit={() => { }} />);
+      const result = render(<Form onSubmit={() => {}} />);
       const input = result.container.querySelector(
         "#location"
       ) as HTMLInputElement;
