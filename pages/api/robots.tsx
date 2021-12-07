90d5743fa200076414a3ac9ec35d5eb9a1c35d13
Etienne Fontaine <etienne.fontaine@blablacar.com>
2021-12-07 09:53:29 +0100

    [feat] Add sitemap on robots.txt

--- pages/api/robots.tsx
+++ pages/api/robots.tsx
@@ -8,7 +8,7 @@ export default async function handler(
 
   res.setHeader("Content-Type", "text/plain");
   if (process.env.DB_ENV && process.env.DB_ENV === "production") {
-    return res.send("User-agent: *\nAllow: /$\nAllow: /fr$\nAllow: /sitemap.xml\nDisallow: /");
+    return res.send("User-agent: *\nAllow: /$\nAllow: /fr$\nAllow: /sitemap.xml\nDisallow: /\nSitemap: https://bonout.com/sitemap.xml");
   }
 
   res.send("User-agent: *\nDisallow: /");
